afd4ed6547acd691ba9d87adf74d759df244aa3d
Dmitriy Khvan <dmitriy.khvan@Dmitriys-MBP.fritz.box>
2020-04-22 19:42:41 +0200

    validation error

--- mmdet/models/detectors/base.py
+++ mmdet/models/detectors/base.py
@@ -85,8 +85,8 @@ class BaseDetector(nn.Module):
         if return_loss:
             return self.forward_train(img, img_meta, **kwargs)
         else:
-            #return self.forward_train(img, img_meta, **kwargs)
-            return self.forward_test(img, img_meta, **kwargs)
+            return self.forward_train(img, img_meta, **kwargs)
+            #return self.forward_test(img, img_meta, **kwargs)
 
     def show_result(self, data, result, dataset=None, score_thr=0.3):
         if isinstance(result, tuple):
