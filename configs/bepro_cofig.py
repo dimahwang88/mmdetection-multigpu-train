aae17701b2fcc7d8c193ceb81717ee3db197da92
Dmitriy Khvan <dmitriy.khvan@Dmitriys-MacBook-Pro.local>
2020-04-22 23:44:13 +0200

    no mask in validation

--- configs/bepro_cofig.py
+++ configs/bepro_cofig.py
@@ -201,7 +201,7 @@ val_pipeline = [
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
 ]
 
 #val_pipeline = [
