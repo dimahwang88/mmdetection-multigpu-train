82dfd5c60567316746b0e372f84fc42e28ced2ac
Dmitriy Khvan <dmitriy.khvan@Dmitriys-MacBook-Pro.local>
2020-04-23 01:33:44 +0200

    20 epochs

--- configs/bepro_cofig.py
+++ configs/bepro_cofig.py
@@ -264,7 +264,7 @@ log_config = dict(
 # yapf:enable
 evaluation = dict(interval=1)
 # runtime settings
-total_epochs = 12
+total_epochs = 20
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/cascade_rcnn_r50_fpn_1x'
